5b15fa970ee6ae67cee49eb2e4f6c89ba81e5330
OscarKevinXR <mtfallsVR@protonmail.com>
2022-06-20 17:34:23 -0400

    Initial Vaultwarden config

--- initial-config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export LOCAL_FILE_PATH=/forest-house-k4/data
-mkdir -p $LOCAL_FILE_PATH
-cp homer/config.yml $LOCAL_FILE_PATH/homer/
-cp vaultwarden/.env $LOCAL_FILE_PATH/vaultwarden/data
