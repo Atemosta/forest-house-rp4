a404de2eadb860b74b624595a63e074022b0cdca
Oscar Kevin <mtfallsVR@atemosta.com>
2022-06-22 10:21:13 -0400

    Update deploy-services.sh

--- deploy-services.sh
+++ deploy-services.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 export APP_DATA_DIR=/forest-house-k4/data
 mkdir -p $APP_DATA_DIR
+
+# Homer Config
 cp homer/config.yml $APP_DATA_DIR/homer/
+sudo chmod 777 $APP_DATA_DIR/homer*
+# Vaulwarden Config
 cp vaultwarden/.env $APP_DATA_DIR/vaultwarden/data
-sudo chmod 777 /forest-house-k4/data*
+sudo chmod 777 $APP_DATA_DIR/vaultwarden/data*
 echo "Successfully ran deploy-services.sh!"
