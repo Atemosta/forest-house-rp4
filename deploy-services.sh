4640fe35a2ac6ac14c83ed41f8e1b5853f09ad3e
Oscar Kevin <mtfallsVR@atemosta.com>
2022-06-22 11:52:20 -0400

    QoL Deployment Updates (#2)

--- deploy-services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export APP_DATA_DIR=/forest-house-k4/data
-mkdir -p $APP_DATA_DIR
-cp homer/config.yml $APP_DATA_DIR/homer/
-cp vaultwarden/.env $APP_DATA_DIR/vaultwarden/data
-echo "Successfully ran deploy-services.sh!"
