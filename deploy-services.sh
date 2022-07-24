a62df88fd0bfec5b354023789328dee46c6ad28b
Oscar Kevin <mtfallsVR@atemosta.com>
2022-07-24 12:32:59 -0400

    Add Matrix Synapse to Docker Deploy (#8)
    * add matrix config
    
    * add matrix config
    
    * add matrix and element to homer
    
    * add matrix and element to homer
    
    * add matrix and element to homer
    
    * add matrix and element to homer
    
    * docker compose updates

--- deploy-services.sh
+++ deploy-services.sh
@@ -6,10 +6,17 @@ export APP_DATA_DIR=/forest-house-k4/data
 mkdir -p $APP_DATA_DIR
 
 # Homer Config
+mkdir -p $APP_DATA_DIR/homer
 cp -r homer/* $APP_DATA_DIR/homer/
 sudo chmod 777 $APP_DATA_DIR/homer*
 
+# Matrix Config
+mkdir -p $APP_DATA_DIR/matrix
+cp matrix/homeserver.yaml $APP_DATA_DIR/matrix/
+sudo chmod 777 $APP_DATA_DIR/matrix*
+
 # Vaultwarden Config
+mkdir -p $APP_DATA_DIR/vaultwarden/data
 cp vaultwarden/.env $APP_DATA_DIR/vaultwarden/data
 sudo chmod 777 $APP_DATA_DIR/vaultwarden/data*
 
